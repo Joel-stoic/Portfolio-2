68789f06b3bf1727bb43359c9681d6ff1eba9fae
Joel-stoic <joelimmanual.off@gmail.com>
2025-06-07 09:35:03 +0530

    Added twitter link

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -12,8 +12,8 @@ const Footer = () => {
           <a href="https://github.com/Joel-stoic"  className="w-1/2 h-1/2"><img src="/assets/github.svg" alt="github" /></a>
           
         </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+        <div className="social-icon">     
+          <a href="https://x.com/joel_immanual?s=21" className="w-1/2 h-1/2"><img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" /></a>
         </div>
         <div className="social-icon">
           <a href="https://www.instagram.com/joel_immanual?igsh=cGE0aWptcGt5aG5y&utm_source=qr" className="w-1/2 h-1/2"><img src="/assets/instagram.svg" alt="instagram"  /></a>
